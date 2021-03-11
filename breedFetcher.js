5937ee4dd9e19ab89ccd785fe1fe7afee0fab698
Zac Alexander <zac@znacreative.com>
2021-03-11 20:22:52 +0000

    Added errors for edge cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,11 +6,15 @@ const catBreedEndpoint = 'https://api.thecatapi.com/v1/breeds/search?name=';
 
 const fetchBreed = (breedType) => {
   request(catBreedEndpoint + breedType, (error, response, body) => {
-    console.error('error:', error);
-    console.log('statusCode:', response && response.statusCode);
-    console.log('body', JSON.parse(body));
-    if (body.length < 3) {
-      console.log(chalk.red('The breed you entered does not exist. Please try another breed, or check your spelling.'));
+    if (error) {
+      console.log(chalk.bgRed('The request could not be completed. Check the URL.'));
+    } else {
+      console.error('error:', error);
+      console.log('statusCode:', response.statusCode);
+      console.log('body', JSON.parse(body));
+      if (body.length < 3) {
+        console.log(chalk.red('The breed you entered does not exist. Please try another breed, or check your spelling.'));
+      }
     }
   });
 };
